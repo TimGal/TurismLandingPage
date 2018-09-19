51757ca4b730f0fb7bb12a54f191bcdf4c6eec5c
Emil Sergeev <admin@firststream.ru>
2018-09-19 16:45:11 +0300

    On change event

--- js/custom-select-file.js
+++ js/custom-select-file.js
@@ -3,7 +3,10 @@
 $(() => {
     $('.file_upload > button.upload')
         .on('click', e => {
-            $('input[type="file"]',e.target.parentElement).click()
-        })
-})
+            $('input[type="file"]', e.target.parentElement).click()
+        });
 
+    $('input[type="file"]').on('change', e => {
+        console.log($(e.target).prop("files"))
+    })
+})
