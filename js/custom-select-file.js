6e382cc174303e3bc1fbb8858cbdaf0e4b8f6de5
TimGal <32193463+TimGal@users.noreply.github.com>
2018-09-19 17:56:19 +0300

    filename extractor

--- js/custom-select-file.js
+++ js/custom-select-file.js
@@ -9,13 +9,22 @@ $(() => {
     $('input[type="file"]').on('change', e => {
         var fileInput = $(e.target);
         var filesList = $('ul.file-contain', e.target.parentElement)
-        console.log(fileInput.prop('files').length)
+        console.log(fileInput.prop('files').length);
         if(fileInput.prop('files').length > 0){
             filesList.html('');
         }
         for(let file of fileInput.prop('files')){
-            console.log(file)
-            filesList.append('<li>'+file.name+'</li>')
+            console.log(file);
+		  	var maxLength = 16;
+		  	var file_length = file.name.lenght;
+		  	var fileName = file.name;
+		  	if (file_length < maxLength) {
+			} else {
+			  var fileLeft = file.name.substring(0, 8);
+			  var fileRight = file.name.substring(file.name.length - 8, file.name.length)
+			  fileName = fileLeft +  "..." + fileRight;
+			}
+            filesList.append('<li>'+fileName+'</li>');
         }
     })
 })
